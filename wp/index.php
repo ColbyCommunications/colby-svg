66b9e58920792100473e29d599d0fbfc8d4e91a9
johnwatkins0 <johnwatkins0@gmail.com>
2017-12-18 09:59:33 -0500

    Don't import autoloader

--- wp/index.php
+++ wp/index.php
@@ -1,11 +1,6 @@
 <?php
 
-// Ensure the autoloader exists.
-if ( ! class_exists( 'ColbyComms\\Svg\\Shortcode' ) ) {
-	require __DIR__ . '/../vendor/autoload.php';
-}
-
-// Make sure we're in a WP environment.
+// Load the shortcode class if we're in a WP environment.
 if ( defined( 'ABSPATH' ) ) {
 	new ColbyComms\SVG\Shortcode();
 }
